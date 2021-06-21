0498bac47a9c61cd74d5479538726dc839e23a94
E.C <emilien.casano@gmail.com>
2021-06-21 19:41:08 +0200

    fix title shadow

--- src/components/config/ConfigShadow.vue
+++ src/components/config/ConfigShadow.vue
@@ -1,9 +1,10 @@
 <template>
   <v-container class="pa-2">
     <v-row no-gutters>
-        <option-title :title="$t('title.shadow.character')"/>
-          <!-- <h4 class="subtitle is-4">Ombre Personnage</h4> -->
         <v-row no-gutters>
+          <v-col cols="12">
+            <option-title :title="$t('title.shadow.character')"/>
+          </v-col>
           <v-col>
             <shadow-setting :title="$t('title.player', {nb: 1})" :i="1" :obj="j1"/>
             <shadow-setting :title="$t('title.versus')" :i="7" :obj="versus"/>
@@ -12,11 +13,11 @@
             <shadow-setting :title="$t('title.player', {nb: 2})" :i="2" :obj="j2"/>
           </v-col>
         </v-row>
-          <!-- <h4 class="subtitle is-4">Ombre Bandeau</h4> -->
-
-        <option-title :title="$t('title.shadow.banner')"/>
 
         <v-row no-gutters>
+          <v-col cols="12">
+            <option-title :title="$t('title.shadow.banner')"/>
+          </v-col>
           <v-col>
             <shadow-setting :title="$t('title.tagPlayer', {nb: 1})" :i="3" :obj="j1.myTag"/>
             <shadow-setting :title="$t('title.phase', {nb: 1})" :i="5" :obj="phase1"/>
